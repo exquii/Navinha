c890ab05b235ddbada50f51723e2431a66539ed8
Gabriel Santos <gabriel.silva.diniz@gmail.com>
2022-06-10 20:05:16 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GameSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Game jogo = new Game();
+           jogo.run();
+        }
+    }
+}
